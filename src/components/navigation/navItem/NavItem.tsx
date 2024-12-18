0b43ac5ea7e9306d84ce96c5da890d7f8a28aa3c
Artem <kamaltema@yandex.ru>
2024-12-18 18:47:06 +0300

    add change color icons Navigation

--- src/components/navigation/navItem/NavItem.tsx
+++ src/components/navigation/navItem/NavItem.tsx
@@ -1,13 +1,14 @@
 import { FC } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import styled from "styled-components"
+import { ReactSVG } from "react-svg"
 
 interface INavItemProps {
 	svgUrl: string,
 	route: string
 }
 
-const NavItemStyled = styled(NavLink)`
+const NavItemStyled = styled(NavLink)<{$isActive: boolean}>`
     width: 90px;
     height: 90px;
     display: flex;
@@ -15,20 +16,29 @@ const NavItemStyled = styled(NavLink)`
     align-items: center;
     cursor: pointer;
     border-radius: 1000px;
-    background: #1C1C1C;
-`;
+    background: ${({ $isActive }) => $isActive ? "white" : "#1C1C1C"}; 
+`
 
-const NavItemImageStyled = styled.img`
+const NavItemImageStyled = styled.div<{$isActive: boolean}>`
     height: 40px;
     width: 40px;
     background: transparent;
-`;
+    path {
+        fill: ${({ $isActive }) => $isActive ? "black" : ""};
+        stroke: ${({ $isActive }) => $isActive ? "black" : ""};
+    }
+`
 
 export const NavItem: FC<INavItemProps> = ({ svgUrl, route }) => {
 
+    const location = useLocation()
+    const isActive = route === "/" ? location.pathname === route : location.pathname.startsWith(route)
+
 	return (
-		<NavItemStyled to={route}>
-			<NavItemImageStyled src={svgUrl} alt="" />
+		<NavItemStyled to={route} $isActive={isActive}>
+			<NavItemImageStyled $isActive={isActive}>
+                <ReactSVG src={svgUrl} />
+            </NavItemImageStyled>
 		</NavItemStyled>
 	)
 }
--- src/config/routes.ts
+++ src/config/routes.ts
@@ -9,10 +9,7 @@ export enum routes {
     doctors = "/staff/doctors",
     techniques = "/staff/techniques",
     typesWork = "/staff/types-work",
-    categoryTechniques = "/staff/category-techniques",
-    addDoctor = "/staff/add-doctor",
-    addTechnique = "/staff/add-technique",
-    addTypeWork = "/staff/add-type-work"
+    categoryTechniques = "/staff/category-techniques"
 }
 
 export enum backendRoutes {
